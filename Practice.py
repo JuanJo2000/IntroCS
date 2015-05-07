3aec051593f9e0062fbe519d67f617b210be3406
JuanJo2000 <jortizjarrin@gmail.com>
2015-05-07 14:35:46 -0500

    Create Practice.py

---
+++ Practice.py
@@ -0,0 +1,16 @@
+name = "Juan Jose"
+last = "Ortiz"
+address = "Samborondon, kilometro 12 isla Mocoli, urvanicasion Monaco"
+email = "jortizjarrin@gmail.com"
+phone = "0997757773"
+
+print name
+print last
+print address
+print email
+print phone
+
+
+
+
+print "My name is", name, "and my last name is", last, "I live in", address, "my email is", email, "and my phone number is", phone, "Thanks"
