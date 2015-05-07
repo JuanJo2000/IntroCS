46f0497efb98963aa224d669446025c26a24f769
JuanJo2000 <jortizjarrin@gmail.com>
2015-05-07 14:36:37 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,15 @@
+operand1 = 95
+operand2 = 64.5
+
+print operand2
+print operand1
+
+print operand1 + operand2
+print operand1 - operand2
+print operand1 * operand2
+print operand1 / operand2
+print operand1 % operand2
+
+# to make amathematical problems using an initialize you create the initialize you want to use in these case 
+# operand1 and operand2 and represent it as the number you want to add, subract, multipy, divide... 
+#Then you use your initialize to make the mathematical problems.
