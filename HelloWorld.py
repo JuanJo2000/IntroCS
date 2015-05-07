1137086d8f8bd429804c3d2439ecf035de5a9d46
JuanJo2000 <jortizjarrin@gmail.com>
2015-05-07 14:34:20 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+print "hellow World"
+print "My name is Juan Jose"
+print "I am 15 years old \nThis is Python"
+
+print 2015 + 1 #Printing on integer
+print "2015" 
+
+print 3.1416 #Printing a floating point number
+
+# Math Operations
+print 1 + 1
+print 1 - 1
+print 1 * 1
+print 1 / 1
