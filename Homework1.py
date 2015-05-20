ef43b6672daa4d88265f23a54434c5c622095c71
JuanJo2000 <jortizjarrin@gmail.com>
2015-05-20 09:55:43 -0500

    Update Homework1.py

--- Homework1.py
+++ Homework1.py
@@ -1,1 +1,7 @@
 
+import sys
+print "your first name is: ", sys.argv[1]
+print "Your last name is: ", sys.argv[2]
+print "Your year of birth is: ", sys.argv[3]
+print "Your year of graduation is: ", sys.argv[4]
+print "Your GPA is: ", sys.argv[5]
